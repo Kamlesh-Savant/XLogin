93d22ad07053157dd208274bf718c82b0a8fcbf2
Kamlesh Savant <sawantk45@gmail.com>
2024-07-27 16:29:04 +0530

    Added login page

--- xlogin/src/App.js
+++ xlogin/src/App.js
@@ -1,8 +1,37 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [msg, setMsg] = useState('');
+  const [success, setSuccess] = useState(false);
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    setMsg('');
+    setSuccess(false);
+    if(userName === "user" && password === "password"){
+      setMsg('Welcome, user!');
+      setSuccess(true);
+    }
+    else{
+      setMsg('Invalid username or password');
+    }
+
+  }
+
   return (
-    <div className="App">
+    <div >
+      <h1>Login Page</h1>
+      <p>{msg}</p>
+      { !success && 
+      <form onSubmit={handleSubmit} >
+        <div><label htmlFor="">Username:</label><input type="text" required value={userName} onChange={(e)=>{setUserName(e.target.value)}}/></div>
+        <div><label htmlFor="">Password:</label><input type="password" required value={password} onChange={(e)=>{setPassword(e.target.value)}}/></div>
+        <button type="submit">Submit</button>
+      </form>
+      }
 
     </div>
   );
